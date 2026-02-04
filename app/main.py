6717971b0224dc7e4d525bb691995d3a94a103d3
zeyad-shaban <zeyadshapan2004@gmail.com>
2026-02-04 07:21:16 +0200

    pre app build

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from modules import Detector, Segmentor
+
+
+def main():
+    detector = Detector()
+    segmentor = Segmentor()
+
+    img_path = "./data/dummy_cats.png"
+
+
+if __name__ == "__main__":
+    main()
